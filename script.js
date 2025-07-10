047b578dcc2a3e20c04c17883948bf95f7456c12
Iakovosathens <iakovosathens@gmail.com>
2025-07-10 14:00:21 +0300

    commit

--- script.js
+++ script.js
@@ -1,29 +1,2 @@
 document.addEventListener("DOMContentLoaded", function () {
   const submenuLinks = document.querySelectorAll(".has-submenu > a");
-
-  submenuLinks.forEach(link => {
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
-      const submenu = this.nextElementSibling;
-
-      // Κλείνουμε όλα τα άλλα υπομενού
-      document.querySelectorAll(".submenu").forEach(menu => {
-        if (menu !== submenu) {
-          menu.classList.remove("show");
-        }
-      });
-
-      // Εναλλαγή εμφάνισης
-      submenu.classList.toggle("show");
-    });
-  });
-
-  // Κλείνουμε τα υπομενού όταν κάνουμε click εκτός
-  document.addEventListener("click", function (e) {
-    if (!e.target.closest(".main-menu")) {
-      document.querySelectorAll(".submenu").forEach(menu => {
-        menu.classList.remove("show");
-      });
-    }
-  });
-});
